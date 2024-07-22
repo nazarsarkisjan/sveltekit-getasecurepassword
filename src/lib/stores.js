04ce748d3d9fe38dd35241a5679cf0bb331684e5
nazarsarkisjan <nazar.sarkisjan@gmail.com>
2024-07-22 12:05:49 +0200

    deleted unused code

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -6,21 +6,3 @@ export const store_symbols = CreateStore("PasswordGenerator.Modifiers.Symbols", 
 export const store_length = CreateStore("PasswordGenerator.Modifiers.Length", 6)
 
 export default { store_letters, store_numbers, store_symbols, store_length };
-
-
-
-// import { browser } from '$app/environment';
-// import { writable } from 'svelte/store';
- 
-// const defaultValue = 'summer';
-// const initialValue = browser ? window.localStorage.getItem('theme') ?? defaultValue : defaultValue;
- 
-// const theme = writable<string>(initialValue);
-
-// theme.subscribe((value) => {
-//   if (browser) {
-//     window.localStorage.setItem('theme', value);
-//   }
-// });
- 
-// export default theme;
