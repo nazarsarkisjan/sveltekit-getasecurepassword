3fbf6d40875a216f1633e954981b95a0bf992498
nazarsarkisjan <nazar.sarkisjan@gmail.com>
2024-07-22 12:05:33 +0200

    temporary local storage

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -4,3 +4,23 @@ export const store_letters = CreateStore("PasswordGenerator.Modifiers.Letters", 
 export const store_numbers = CreateStore("PasswordGenerator.Modifiers.Numbers", false)
 export const store_symbols = CreateStore("PasswordGenerator.Modifiers.Symbols", false)
 export const store_length = CreateStore("PasswordGenerator.Modifiers.Length", 6)
+
+export default { store_letters, store_numbers, store_symbols, store_length };
+
+
+
+// import { browser } from '$app/environment';
+// import { writable } from 'svelte/store';
+ 
+// const defaultValue = 'summer';
+// const initialValue = browser ? window.localStorage.getItem('theme') ?? defaultValue : defaultValue;
+ 
+// const theme = writable<string>(initialValue);
+
+// theme.subscribe((value) => {
+//   if (browser) {
+//     window.localStorage.setItem('theme', value);
+//   }
+// });
+ 
+// export default theme;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,26 @@
 <script>
+    import { store_letters, store_numbers, store_symbols, store_length } from '$lib/stores.js';
     import { onMount } from 'svelte';
     import "../app.scss";
 
     let finalPassword = "";
-    let passwordLength = 12;
+    // let passwordLength = $store_length;
 
     const modifiers = {
         letters: {
             "alias": "Letters",
             "values": "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWVXYZ".split(""),
-            "enabled": true,
+            "enabled": $store_letters,
         },
         numbers: {
             "alias": "Numbers",
             "values": "1234567890".split(""),
-            "enabled": true,
+            "enabled": $store_numbers,
         },
         symbols: {
             "alias": "Symbols",
             "values": `!@#$%^&*();:'"\|.,`.split(""),
-            "enabled": false,
+            "enabled": $store_symbols,
         }
     }
 
@@ -28,7 +29,7 @@
 
         setTimeout(() => {
             copyPassword();
-        }, 10);
+        }, 5);
     });
 
     function generatePassword() {
@@ -40,7 +41,10 @@
             let collection = [];
             
             choices.forEach(e => {
+                console.log(e);
                 if (e[1].enabled) {
+
+
                     e[1].values.forEach(e => {
                         collection = collection.concat(e);
                     });
@@ -53,7 +57,7 @@
             return randomToken;
         }
 
-        for (let index = 0; index < passwordLength; index++) {
+        for (let index = 0; index < $store_length; index++) {
             finalPassword = `${finalPassword}${getRandomToken()}`;
         }
     }
@@ -68,13 +72,26 @@
 </script>
 
 <div class="main">
-    <button on:click={() => copyPassword()}>
-        <input type="text" value={finalPassword} readonly id="password">
-    </button>
-
-    <button on:click={() => generatePassword()}>generate</button>
-
-    <input type="number" min="6" bind:value={passwordLength}>
+    <div class="main__restrict">
+        <button class="main__password" on:click={() => copyPassword() }>
+            {finalPassword}
+            <input type="text" value={finalPassword} readonly id="password">
+        </button>
+    
+        <button class="main__generate" on:click={() => { generatePassword(); setTimeout(() => { copyPassword() }, 5); }}>Generate</button>
+    
+        <div class="main__characters">
+            <div>
+                <input type="checkbox" name="numbers" id="numbers" bind:checked={$store_numbers}>
+                <label for="numbers">numbers</label>
+            </div>
+            <div>
+                <input type="checkbox" name="symbols" id="symbols" bind:checked={$store_symbols}>
+                <label for="symbols">symbols</label>
+            </div>
+            <input class="characters" type="number" min="6" bind:value={$store_length}>
+        </div>
+    </div>
 </div>
 
 <slot></slot>
@@ -82,12 +99,83 @@
 <style lang="scss">
     .main {
         width: 100%;
+        height: 90vh;
         display: flex;
         justify-content: center;
         align-items: center;
 
-        input {
-            text-align: center; 
+        .main__restrict {
+            width: 100%;
+            max-width: 300px;
+            padding: 10px;
+
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+
+            border-radius: 5px;
+
+            .main__password {
+                text-align: center;
+                color: white;
+                background-color: rgb(49, 47, 47);
+                border-radius: 5px;
+                padding: 10px 15px;
+                border: none;
+
+                -webkit-user-select: text;
+                -moz-user-select: text;
+                -ms-user-select: text;
+                user-select: text;
+
+                text-overflow: ellipsis;
+                overflow: hidden;
+                
+                input {
+                    display: none;
+                }
+            }
+    
+            .main__generate {
+                color: white;
+                text-transform: capitalize;
+                background-color: rgb(85, 85, 85);
+
+                padding: 10px 15px;
+                border-radius: 5px;
+                border: none;
+
+                cursor: pointer;
+            }
+    
+            .main__characters {
+                display: flex;
+                justify-content: space-between;
+
+                div {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    gap: 2px;
+
+                    text-transform: capitalize;
+
+                    label {
+                        color: white;
+                    }
+                }
+
+                .characters {
+                    width: 50px;
+                    padding: 8px;
+
+                    background-color: rgb(49, 47, 47);
+                    color: white;
+
+                    border-radius: 5px;
+                    border: none;
+                }
+            }
         }
     }
 </style>
